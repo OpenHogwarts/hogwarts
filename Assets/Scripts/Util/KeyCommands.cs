3b60aa204b1c987328394aa4a8f7e21e6f3e7567
Vancete <vancete07@gmail.com>
2015-01-06 18:35:57 +0100

    + Added script (Z hides HUD, F2 to do a screenshot at 4x res)

---
+++ Assets/Scripts/Util/KeyCommands.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyCommands : MonoBehaviour {
+
+	public GameObject canvas;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Z)) {
+			if(canvas.GetActive()){
+				canvas.SetActive(false);
+			}else{
+				canvas.SetActive(true);
+			}
+		}else if (Input.GetKeyDown (KeyCode.F2)) {
+			Application.CaptureScreenshot(string.Format("Captura{0}.png", System.DateTime.Now.ToString("_d-MMM-yyyy-HH-mm-ss-f")), 4);
+		}
+
+	}
+}
