ac3e87cda92d5b64db2d34b7db170328c6f3dd31
Vancete <vancete07@gmail.com>
2015-01-09 01:10:53 +0100

    = Major UI update +Added config menu (disconnect, graphic options, quit)

---
+++ Assets/Scripts/UI/ConfigMenu.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ConfigMenu : MonoBehaviour {
+
+	public GameObject menu;
+	public GameObject options;
+	public Toggle fxaa;
+	public Toggle ssao;
+
+	public void ConfigShow(){
+		if (menu.GetActive()) {
+			menu.SetActive(false);
+		} else {
+			menu.SetActive(true);
+		}
+	}
+
+	public void ConfigDisconnect(){
+		PhotonNetwork.Disconnect ();
+		Application.LoadLevel ("MainMenu");
+	}
+
+	public void ConfigOptions(){
+		menu.SetActive (false);
+		options.SetActive (true);
+	}
+
+	public void ConfigQuit(){
+		Application.Quit ();
+	}
+
+	public void OptionsQuit(){
+		options.SetActive (false);
+	}
+
+	public void OptionsSetQuality(int q){
+		QualitySettings.SetQualityLevel (q);
+	}
+
+	public void OptionsSetFXAA(){
+		Camera.main.GetComponent<AntialiasingAsPostEffect> ().enabled = fxaa.isOn;
+	}
+
+	public void OptionsSetSSAO(){
+		Camera.main.GetComponent<AmbientObscurance> ().enabled = ssao.isOn;
+	}
+}
