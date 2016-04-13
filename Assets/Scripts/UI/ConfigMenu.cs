3a1bf26d75aeca8671ab4357fc57e2082c90697a
Vancete <vancete07@gmail.com>
2016-04-13 18:28:07 +0200

    -Settings menu rework

--- Assets/Scripts/UI/ConfigMenu.cs
+++ Assets/Scripts/UI/ConfigMenu.cs
@@ -1,27 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityStandardAssets.CinematicEffects;
 
 public class ConfigMenu : MonoBehaviour {
 
 	public GameObject menu;
-	public GameObject options;
-	public Toggle fxaa;
+	public Dropdown qdrop;
 	public Toggle ssao;
 	public Toggle dof;
-	public Toggle rbt;
 	public Toggle performance;
 	public GameObject rightbar;
 	public GameObject perftext;
-	public Slider uiscale;
-	public Canvas ui;
+
+
+	void LoadCfg(){
+		ssao.isOn = Camera.main.GetComponent<SESSAO> ().enabled;
+		dof.isOn = Camera.main.GetComponent<DepthOfField> ().enabled;
+		qdrop.value = QualitySettings.GetQualityLevel ();
+	}
 
 	public void ConfigShow(){
-		if (menu.GetActive()) {
-			menu.SetActive(false);
-		} else {
-			menu.SetActive(true);
-		}
+		menu.SetActive(!menu.activeSelf);
+		LoadCfg ();
 	}
 
 	public void ConfigDisconnect(){
@@ -29,41 +30,24 @@ public class ConfigMenu : MonoBehaviour {
 		Application.LoadLevel ("MainMenu");
 	}
 
-	public void ConfigOptions(){
-		menu.SetActive (false);
-		options.SetActive (true);
-	}
-
 	public void ConfigQuit(){
 		Application.Quit ();
 	}
 
-	public void OptionsQuit(){
-		options.SetActive (false);
-	}
-
-	public void OptionsSetQuality(int q){
-		QualitySettings.SetQualityLevel (q);
-	}
-
-	public void OptionsSetFXAA(){
-		//Camera.main.GetComponent<AntialiasingAsPostEffect> ().enabled = fxaa.isOn;
+	public void OptionsSetQuality(){
+		QualitySettings.SetQualityLevel (qdrop.value);
 	}
 
 	public void OptionsSetSSAO(){
-		//Camera.main.GetComponent<AmbientObscurance> ().enabled = ssao.isOn;
+		Camera.main.GetComponent<SESSAO> ().enabled = ssao.isOn;
 	}
 
 	public void OptionsSetDOF(){
-		//Camera.main.GetComponent<DepthOfFieldScatter> ().enabled = dof.isOn;
-	}
-
-	public void OptionsSetUIScale(){
-		ui.scaleFactor = uiscale.value;
+		Camera.main.GetComponent<DepthOfField> ().enabled = dof.isOn;
 	}
 
 	public void OptionsRightBar(){
-		rightbar.SetActive (rbt.isOn);
+		//rightbar.SetActive ();
 	}
 
 	public void OptionsPerformance(){
