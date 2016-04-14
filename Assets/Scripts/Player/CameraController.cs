99f10fe39e9971f5c9d257ff4bf7f2579668d172
Vancete <vancete07@gmail.com>
2016-04-14 14:52:06 +0200

    +Added camera collision detection

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -19,6 +19,9 @@ public class CameraController : MonoBehaviour {
 	public float correctedDistance;
 	public float currentDistance;
 	private float oldDistance;
+	private bool isHitting = false;
+	public float lastDistance;
+	private bool reachedDist = true;
 
 	public float cameraTargetHeight = 1.0f;
 
@@ -64,6 +67,21 @@ public class CameraController : MonoBehaviour {
 
 		transform.rotation = rotation;
 		transform.position = position;
+
+		if (isHitting) {
+			desiredDistance -= 0.01f * Time.deltaTime * zoomRate * Mathf.Abs (desiredDistance);
+			desiredDistance = Mathf.Clamp (desiredDistance, minViewDistance, maxViewDistance);
+		} else {
+			Debug.DrawLine(transform.position - (transform.forward * 0.5f), transform.position - (transform.forward * (lastDistance-desiredDistance)));
+			if (desiredDistance < lastDistance) {
+				if ((!Physics.Raycast (transform.position - (transform.forward * 0.5f), -transform.forward, (lastDistance - desiredDistance)))&&(!Physics.Raycast(transform.position - (transform.forward * ((lastDistance - desiredDistance)+0.5f)), Vector3.down, 0.5f)))  {
+					desiredDistance += 0.01f * Time.deltaTime * zoomRate * Mathf.Abs (desiredDistance);
+					desiredDistance = Mathf.Clamp (desiredDistance, minViewDistance, maxViewDistance);
+				}
+			} else {
+				reachedDist = true;
+			}
+		}
 	}
 
 	// set play camera preferences before quit
@@ -81,6 +99,22 @@ public class CameraController : MonoBehaviour {
 		}
 
 		return Mathf.Clamp (angle, min, max);
+	}
+
+	void OnTriggerEnter(){
+		isHitting = true;
+		if (reachedDist) {
+			lastDistance = currentDistance;
+			reachedDist = false;
+		}
+	}
+
+	void OnTriggerStay(){
+		isHitting = true;
+	}
+
+	void OnTriggerExit(){
+		isHitting = false;
 	}
 
 }
