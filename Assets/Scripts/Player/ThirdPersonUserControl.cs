2ecc297d676b292d76a0604e46682b007a9eb671
Vancete <vancete07@gmail.com>
2016-04-15 21:14:17 +0200

    -TPC minor changes

--- Assets/Scripts/Player/ThirdPersonUserControl.cs
+++ Assets/Scripts/Player/ThirdPersonUserControl.cs
@@ -12,7 +12,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         private Vector3 m_CamForward;             // The current forward direction of the camera
         private Vector3 m_Move;
         private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
-
+		private float lastInput = 0;
         
         private void Start()
         {
@@ -56,9 +56,12 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             // calculate move direction to pass to character
             if (m_Cam != null)
             {
-                // calculate camera relative direction to move:
-				m_CamForward = Vector3.Scale(gameObject.transform.forward, new Vector3(1, 0, 1)).normalized;
-                m_Move = v*m_CamForward + h*m_Cam.right;
+				if ((lastInput != v)||(h != 0)) {
+					// calculate camera relative direction to move:
+					m_CamForward = Vector3.Scale (m_Cam.forward, new Vector3 (1, 0, 1)).normalized;
+
+				}
+				m_Move = v * m_CamForward + h * m_Cam.right;
             }
             else
             {
@@ -73,6 +76,8 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             // pass all parameters to the character control script
             m_Character.Move(m_Move, crouch, m_Jump);
             m_Jump = false;
+
+			lastInput = v;
         }
     }
 }
