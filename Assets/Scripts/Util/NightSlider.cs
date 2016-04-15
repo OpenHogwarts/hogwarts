7511369cb2439601693dd813665d9c31cb230e47
Vancete <vancete07@gmail.com>
2016-04-15 18:46:08 +0200

    +NightSlider (slides between day and night light)

---
+++ Assets/Scripts/Util/NightSlider.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NightSlider : MonoBehaviour {
+
+	public GameObject mainLight;
+	public Color mainLightDay;
+	public Color mainLightNight;
+	public GameObject secLight;
+	public Color secLightDay;
+	public Color secLightNight;
+	[Range(0.0f, 1.0f)]
+	public float slider = 0f;
+	
+	// Update is called once per frame
+	void Update () {
+		mainLight.GetComponent<Light> ().color = Color.Lerp (mainLightDay, mainLightNight, slider);
+		secLight.GetComponent<Light> ().color = Color.Lerp (secLightDay, secLightNight, slider);
+	}
+}
