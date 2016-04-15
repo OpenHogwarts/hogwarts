bd8e458eb679d9556be331c17e778f725f34a192
Vancete <vancete07@gmail.com>
2016-04-15 19:04:19 +0200

    -Improved NightSlider

--- Assets/Scripts/Util/NightSlider.cs
+++ Assets/Scripts/Util/NightSlider.cs
@@ -9,6 +9,8 @@ public class NightSlider : MonoBehaviour {
 	public GameObject secLight;
 	public Color secLightDay;
 	public Color secLightNight;
+	public float dayAngle = 68f;
+	public float nightAngle = 38f;
 	[Range(0.0f, 1.0f)]
 	public float slider = 0f;
 	
@@ -16,5 +18,7 @@ public class NightSlider : MonoBehaviour {
 	void Update () {
 		mainLight.GetComponent<Light> ().color = Color.Lerp (mainLightDay, mainLightNight, slider);
 		secLight.GetComponent<Light> ().color = Color.Lerp (secLightDay, secLightNight, slider);
+		mainLight.transform.localEulerAngles = Vector3.Lerp (new Vector3 (dayAngle, 200, 0), new Vector3 (nightAngle, 200, 0), slider);
+		RenderSettings.skybox.SetFloat("_Exposure", Mathf.Lerp(1f, 0.75f, slider));
 	}
 }
