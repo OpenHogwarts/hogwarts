105da54cc5e279916edb9cf6bb3b61c24ccd447b
Vancete <vancete07@gmail.com>
2016-04-27 01:57:54 +0200

    +Added intro movie

---
+++ Assets/Scripts/UI/Intro.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Intro : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+		GetComponent<AudioSource> ().Play ();
+		StartCoroutine ("WaitForMovie");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown) {
+			Application.LoadLevel ("MainMenu");
+		}
+	}
+
+	IEnumerator WaitForMovie(){
+		yield return new WaitForSeconds (10);
+		Application.LoadLevel ("MainMenu");
+	}
+}
