7ec26d72298380567bfe3def2bf6b41390d69b8d
Vancete <vancete07@gmail.com>
2016-04-27 20:31:57 +0200

    +Added Book system

---
+++ Assets/Scripts/UI/OpenBook.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenBook : MonoBehaviour {
+
+
+	public int book;
+
+	void OnMouseDown () {
+		GameObject.Find ("Canvas/Books/Panel").SetActive (true);
+		GameObject.Find ("Canvas/Books/Base").SetActive (true);
+		GameObject.Find("Canvas/Books/Book"+book).SetActive (true);
+	}
+}
