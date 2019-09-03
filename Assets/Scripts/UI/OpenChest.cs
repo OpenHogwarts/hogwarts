2ab107d39a181d07f5e5374c98d306a89ac4b02d
finchen <antoine.spodobalski@gmail.com>
2019-09-04 09:33:50 +1200

    a chest that opens (not used in any quest) and a mech collider onb the floor of the stadium

---
+++ Assets/Scripts/UI/OpenChest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(BoxCollider))]
+public class OpenChest : MonoBehaviour
+{
+
+    private Animator anim;
+
+
+    private bool isLocked = false;
+
+    private bool isOpen = false;
+
+    private void Start()
+    {
+        this.anim = this.GetComponent<Animator>();
+    }
+
+    void OnMouseDown()
+    {
+        if (isLocked) {
+            // TODO later. Magic spell to open it
+        } else if (this.isOpen == false) {
+            this.anim.SetTrigger("open");
+            this.isOpen = true;
+        } else if (this.isOpen) {
+            this.anim.SetTrigger("close");
+            this.isOpen = false;
+        }
+    }
+
+}
