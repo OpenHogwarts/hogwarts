3d6d7ea683c63c492754e40971043ad1802229c4
Kenta Lange <kenta.ozawa.lange@gmail.com>
2022-05-16 23:30:33 -0400

    Week 2 Wise Monday
    Started to make player object: moving left and right

--- Wise/Wise/Assets/Scripts/ChangeCamera.cs
+++ Wise/Wise/Assets/Scripts/ChangeCamera.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ChangeCamera : MonoBehaviour
 {
 
     // Start is called before the first frame update
@@ -15,45 +15,45 @@ public class NewBehaviourScript : MonoBehaviour
     void Update()
     {
       if(Input.GetButtonDown("key1")==true )
-      {
+      {//If key 1 is pressed, activate camera 2 and turn off others
         cam1.SetActive(false);
         cam2.SetActive(true);
         cam3.SetActive(false);
         cam4.SetActive(false);
-        Debug.Log("Space key was pressed down");
+        //Debug.Log("Space key was pressed down");
 
       }
 
       if(Input.GetButtonDown("key2")==true)
-      {
+      {//If key 2 is pressed, activate camera 3 and turn off others
         cam1.SetActive(false);
         cam2.SetActive(false);
         cam3.SetActive(true);
         cam4.SetActive(false);
-        Debug.Log("Space key was pressed down1");
+        //Debug.Log("Space key was pressed down1");
 
       }
 
       if(Input.GetButtonDown("key3")==true)
-      {
+      {//If key 3 is pressed, activate camera 4 and turn off others
         cam1.SetActive(false);
         cam2.SetActive(false);
         cam3.SetActive(false);
         cam4.SetActive(true);
-        Debug.Log("Space key was pressed down2");
+        //Debug.Log("Space key was pressed down2");
 
       }
 
       if(Input.GetButtonDown("key4")==true)
-      {
-        cam1.SetActive(false);
-        cam2.SetActive(true);
+      {//If key 4 is pressed, activate camera 1 and turn off others
+        cam1.SetActive(true);
+        cam2.SetActive(false);
         cam3.SetActive(false);
         cam4.SetActive(false);
-        Debug.Log("Space key was pressed down3");
+        //Debug.Log("Space key was pressed down3");
 
       }
-      
+
     }
 
 
---
+++ Wise/Wise/Assets/Scripts/player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed=1.0f;
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      //If the left key is pressed, the object moves by
+
+      if (Input.GetKey(KeyCode.LeftArrow))
+      {
+         if(this.transform.position.x >= -23.1)
+         {
+           this.transform.position+= Vector3.left * speed * Time.deltaTime;
+         }
+
+      }
+
+      if(Input.GetKey(KeyCode.RightArrow))
+      {
+        Debug.Log(this.transform.position);
+         if(this.transform.position.x <= 23.5)
+         {
+
+           this.transform.position = this.transform.position + new Vector3 (240f,0f,0f) *  Time.deltaTime;
+
+         }
+      }
+    }
+}
